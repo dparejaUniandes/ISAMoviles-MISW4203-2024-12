4eef3ab4452b842385b5d530901b6f4d62a49022
GeraldineGomez <gs.gomezm1@uniandes.edu.co>
2024-04-27 09:27:40 -0500

    feat: created collector, comment and album services

---
+++ app/src/main/java/com/example/vinilosapp/service/AlbumService.kt
@@ -0,0 +1,13 @@
+package com.example.vinilosapp.service
+
+import com.example.vinilosapp.models.Album
+import com.example.vinilosapp.models.Comment
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface AlbumService {
+    @GET("albums")
+    fun getAlbums():
+            Call<List<Album>>
+}
