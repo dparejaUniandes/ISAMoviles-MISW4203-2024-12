066d0995a2ef9a26271ab29402113927927148a4
GeraldineGomez <avocadoenterprise@gmail.com>
2024-04-28 19:59:01 -0500

    feat: added title bar with the album's name

--- app/src/main/java/com/example/vinilosapp/view/AlbumDetailFragment.kt
+++ app/src/main/java/com/example/vinilosapp/view/AlbumDetailFragment.kt
@@ -2,20 +2,22 @@ package com.example.vinilosapp.view
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.vinilosapp.viewmodels.AlbumDetailViewModel
 import com.example.vinilosapp.databinding.AlbumDetailFragmentBinding
 import com.example.vinilosapp.models.Album
 import com.example.vinilosapp.view.adapters.AlbumDetailAdapter
+import com.example.vinilosapp.viewmodels.AlbumDetailViewModel
+
 
 class AlbumDetailFragment : Fragment() {
 
@@ -47,7 +49,6 @@ class AlbumDetailFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         val activity = requireNotNull(this.activity)
-        this.activity?.actionBar?.title = "addada"
         val args: AlbumDetailFragmentArgs by navArgs()
         Log.d("Args", args.albumId.toString())
         viewModel = ViewModelProvider(this, AlbumDetailViewModel.Factory(activity.application, args.albumId)).get(
@@ -56,7 +57,7 @@ class AlbumDetailFragment : Fragment() {
             it.apply {
                 if(this.albumId == albumId){
                     viewModelAdapter!!.album = this
-                    activity.actionBar?.title = this.name
+                    (getActivity() as AppCompatActivity?)!!.supportActionBar!!.title = this.name
                 }
             }
         })
