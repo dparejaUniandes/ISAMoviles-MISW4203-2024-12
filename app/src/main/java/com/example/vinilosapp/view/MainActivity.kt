036e9e5cd57a59578c0f86e31746ad0ae1cff93f
dparejaUniandes <d.pareja@uniandes.edu.co>
2024-05-01 17:05:27 -0500

    Try to fix the bottom menu

--- app/src/main/java/com/example/vinilosapp/view/MainActivity.kt
+++ app/src/main/java/com/example/vinilosapp/view/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.vinilosapp.view
 
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.vinilosapp.R
 import com.example.vinilosapp.databinding.ActivityMainBinding
+import com.example.vinilosapp.models.Album
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -26,6 +31,38 @@ class MainActivity : AppCompatActivity() {
         Log.d("act", navController.toString())
         setSupportActionBar(findViewById(R.id.my_toolbar))
         setupActionBarWithNavController(navController)
+
+
+
+
+        // FOR THE BOTTOM MENU - this can be deleted
+        val navListener =
+            BottomNavigationView.OnNavigationItemSelectedListener { item: MenuItem ->
+
+                R.id.albumsRv
+                R.id.artistsRv
+
+                Log.d("Item id", item.itemId.toString())
+                var selectedFragment: Fragment? = null
+                val itemId = item.itemId
+                if (itemId == 2131296332) {
+                    selectedFragment = AlbumFragment()
+                } else if (itemId == 2131296344) {
+                    selectedFragment = ArtistFragment()
+                }
+                // It will help to replace the
+                // one fragment to other.
+                if (selectedFragment != null) {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment, selectedFragment).commit()
+                }
+                true
+            }
+
+        val bottomNav : BottomNavigationView = findViewById(R.id.bottomNav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+
 
     }
 
