cfb5dd5b2a89415a285e418fc7c4d85fd0b430f4
dparejaUniandes <d.pareja@uniandes.edu.co>
2024-05-10 18:30:59 -0500

    Added corrutinas for the HU01 and HU02 use case

--- app/src/main/java/com/example/vinilosapp/viewmodels/AlbumDetailViewModel.kt
+++ app/src/main/java/com/example/vinilosapp/viewmodels/AlbumDetailViewModel.kt
@@ -1,13 +1,18 @@
 package com.example.vinilosapp.viewmodels
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.example.vinilosapp.models.Album
 import com.example.vinilosapp.repository.AlbumRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AlbumDetailViewModel(application: Application, albumId: Int) :  AndroidViewModel(application) {
 
@@ -33,13 +38,18 @@ class AlbumDetailViewModel(application: Application, albumId: Int) :  AndroidVie
     }
 
     private fun refreshDataFromNetwork() {
-        albumRepository.refreshData(id, {
-            _album.postValue(it)
-            _eventNetworkError.value = false
-            _isNetworkErrorShown.value = false
-        },{
-            _eventNetworkError.value = true
-        })
+        viewModelScope.launch(Dispatchers.Default) {
+            withContext(Dispatchers.IO) {
+                albumRepository.refreshData(id).onSuccess {
+                    _album.postValue(it)
+                    _eventNetworkError.postValue(false)
+                    _isNetworkErrorShown.postValue(false)
+                }.onFailure {
+                    Log.d("Error", it.toString())
+                    _eventNetworkError.postValue(true)
+                }
+            }
+        }
     }
 
     fun onNetworkErrorShown() {
