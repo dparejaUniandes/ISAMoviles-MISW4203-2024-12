660a825124eb4a16c93a07d4947d056d46d876a0
dparejaUniandes <d.pareja@uniandes.edu.co>
2024-05-10 21:43:40 -0500

    Feature/caching (#40)
    * Added corutine for the artist detail
    
    * Added corutine for the artist list
    
    * Created the caching for HU04
    
    * Added corrutinas for the HU01 and HU02 use case
    
    * Added corrutine for the collector service

---
+++ app/src/main/java/com/example/vinilosapp/broker/CacheManager.kt
@@ -0,0 +1,26 @@
+package com.example.vinilosapp.broker
+
+import android.content.Context
+import androidx.collection.LruCache
+import com.example.vinilosapp.models.Artist
+
+class CacheManager(context: Context) {
+    companion object{
+        var instance: CacheManager? = null
+        fun getInstance(context: Context) =
+            instance ?: synchronized(this) {
+                instance ?: CacheManager(context).also {
+                    instance = it
+            }
+        }
+    }
+    private var artistDetail: LruCache<Int, Artist> = LruCache(3)
+    fun addArtistDetail(artistId: Int, artistDetailNew: Artist){
+        if (artistDetail[artistId] == null){
+            artistDetail.put(artistId, artistDetailNew)
+        }
+    }
+    fun getArtistDetail(artistId: Int) : Artist {
+        return if (artistDetail[artistId]!=null) artistDetail[artistId]!! else Artist(artistId=-1)
+    }
+}
