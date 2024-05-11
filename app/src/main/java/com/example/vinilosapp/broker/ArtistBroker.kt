6ff7a8c4c48db016aaec170f4a5207b0be2e5b8d
GeraldineGomez <avocadoenterprise@gmail.com>
2024-05-11 14:44:26 -0500

    fix: removed unnecessary dependencies

--- app/src/main/java/com/example/vinilosapp/broker/ArtistBroker.kt
+++ app/src/main/java/com/example/vinilosapp/broker/ArtistBroker.kt
@@ -1,10 +1,7 @@
 package com.example.vinilosapp.broker
 
 import android.content.Context
-import android.util.Log
 import com.example.vinilosapp.models.Artist
-import retrofit2.Call
-import retrofit2.Callback
 import java.lang.Exception
 
 class ArtistBroker constructor(context: Context) {
