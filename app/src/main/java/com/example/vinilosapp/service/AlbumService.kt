b485b8a87fce86bf4f1ad1a1908002c0bced7dff
GeraldineGomez <avocadoenterprise@gmail.com>
2024-05-11 14:48:25 -0500

    fix: removed unnecessary dependencies

--- app/src/main/java/com/example/vinilosapp/service/AlbumService.kt
+++ app/src/main/java/com/example/vinilosapp/service/AlbumService.kt
@@ -1,8 +1,6 @@
 package com.example.vinilosapp.service
 
 import com.example.vinilosapp.models.Album
-import com.example.vinilosapp.models.Comment
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
