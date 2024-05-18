717e9aef86297b8c88533b26354133a6524f7f15
dparejaUniandes <d.pareja@uniandes.edu.co>
2024-05-18 17:02:14 -0500

    When album is created, can visualized in the albums view

--- app/src/main/java/com/example/vinilosapp/view/AlbumCreateFragment.kt
+++ app/src/main/java/com/example/vinilosapp/view/AlbumCreateFragment.kt
@@ -8,9 +8,9 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.vinilosapp.R
 import com.example.vinilosapp.viewmodels.AlbumCreateViewModel
-import com.example.vinilosapp.viewmodels.AlbumDetailViewModel
 import com.google.android.material.textfield.TextInputEditText
 
 
@@ -27,16 +27,8 @@ private const val ARG_PARAM2 = "param2"
 class AlbumCreateFragment : Fragment() {
     private lateinit var viewModel: AlbumCreateViewModel
 
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -70,26 +62,11 @@ class AlbumCreateFragment : Fragment() {
                 "recordLabel" to recordLabelTxt.text.toString()
             )
             viewModel.refreshDataFromNetwork(postParams)
-        }
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AlbumCreateFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AlbumCreateFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+            Thread.sleep(2000)
+            val action = AlbumCreateFragmentDirections.actionAlbumCreateFragmentToAlbumFragment()
+            // Navigate using that action
+            view.findNavController().navigate(action)
+        }
     }
 }
